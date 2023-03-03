89f071ef350f1233effa5ad563c634074f56c12d
Fábio <fabioalmeida0896@gmail.com>
2023-03-03 19:37:23 -0300

    add --needed flag on bootstrap script

--- roles/arch/scripts/bootstrap.sh
+++ roles/arch/scripts/bootstrap.sh
@@ -3,6 +3,6 @@ cd $HOME
 
 git clone https://github.com/fabioalmeida08/arch_playbook
 
-sudo pacman -Syu ansible ansible-core --noconfirm
+sudo pacman -Syu --needed ansible ansible-core --noconfirm
 
 ansible-playbook $HOME/arch_playbook/local.yml -K
