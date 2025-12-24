4dc822eaa346c01669a20c4f4abb7161e2b80031
fabio <fabioalmeida0896@gmail.com>
2025-12-24 16:01:13 -0300

    fix: changed cmd cd in the script

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,12 +18,12 @@ check_requirements () {
 }
 
 run_playbook () {
-    cd "$( dirname "$0" )"
+    cd arch_playbook
     time ansible-playbook local.yml -K
 }
 
 install_yay () {
-    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd
 }
 
 sudo pacman -Syu
